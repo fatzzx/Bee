22a56f3ed06b50ef802b5ec6d3e7b3cf46afe858
fatzzx <felipespinolafarias1@gmail.com>
2023-04-04 23:20:54 -0300

    feat: :sparkles: Beecrowd 3059 exercise

---
+++ 3059.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (){
+
+    int n, ini, f, contador =0, soma;
+    scanf("%d %d %d", &n, &ini, &f);
+    int v[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            soma = v[i] + v[j]; 
+            if(soma >= ini && soma <= f ){
+                contador++;
+            }
+        }
+    }
+    printf("%d\n", contador);
+
+
+    return 0;
+}
